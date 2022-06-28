5fc88dbe92132b1e3867c8bccb2502207ec10c79
holdennekt <ngryshchak@gmail.com>
2022-06-28 15:38:42 +0300

    add task struct

--- pkg/dataProviders/connection.go
+++ pkg/dataProviders/connection.go
@@ -8,7 +8,16 @@ import (
 )
 
 type DataProvider struct {
-	db sql.DB
+	db *sql.DB
+}
+
+func NewDataProvider(conn Connection) (*DataProvider, error) {
+	url := conn.String()
+	db, err := sql.Open("postgres", url)
+	if err != nil {
+		return nil, err
+	}
+	return &DataProvider{db}, nil
 }
 
 type Connection struct {
@@ -35,14 +44,12 @@ func (c *Connection) String() string {
 	return dbUrl.String()
 }
 
-func NewDataProvider(conn Connection) (*DataProvider, error) {
-	url := conn.String()
-	db, err := sql.Open("postgres", url)
-	if err != nil {
-		return nil, err
-	}
-	return &DataProvider{db: *db}, nil
-}
-
 type Task struct {
+	Id          int
+	Title       string
+	Details     sql.NullString
+	Deadline    sql.NullTime
+	Expired     bool
+	Completed   bool
+	CompletedAt sql.NullTime
 }
---
+++ pkg/dataProviders/listTasks.go
@@ -0,0 +1,39 @@
+package dataproviders
+
+func (dp *DataProvider) getTasks(sqlQuery string) ([]*Task, error) {
+	rows, err := dp.db.Query(sqlQuery)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	tasks := make([]*Task, 0)
+	for rows.Next() {
+		var t Task
+		err := rows.Scan(
+			&t.Id,
+			&t.Title,
+			&t.Details,
+			&t.Deadline,
+			&t.Expired,
+			&t.Completed,
+			&t.CompletedAt)
+		if err != nil {
+			return nil, err
+		}
+		tasks = append(tasks, &t)
+	}
+	return tasks, nil
+}
+
+func (dp *DataProvider) ListAllTasks() ([]*Task, error) {
+	return dp.getTasks("select * from tasks;")
+}
+
+func (dp *DataProvider) ListActiveTasks() ([]*Task, error) {
+	return dp.getTasks("select * from tasks where completed=false order by deadline asc;")
+}
+
+func (dp *DataProvider) ListExpiredTasks() ([]*Task, error) {
+	return dp.getTasks("select * from tasks where expired=true order by deadline asc;")
+}
