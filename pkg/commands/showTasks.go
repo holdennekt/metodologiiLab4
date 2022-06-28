787da795f438b9cc000bfc5079f0f5cea9bb9a61
BogPin <bogdanpin2002@gmail.com>
2022-06-28 15:53:15 +0300

    implement showTasksCommand

--- pkg/commands/showTasks.go
+++ pkg/commands/showTasks.go
@@ -1,46 +1,94 @@
 package commands
 
 import (
-	"flag"
-	"fmt"
+ "flag"
+ "fmt"
+
+ dataproviders "github.com/holdennekt/metodologiiLab4/pkg/dataProviders"
 )
 
 type showTasksCommand struct {
-	fs             flag.FlagSet
-	all, todo, exp bool
+ fs             flag.FlagSet
+ all, todo, exp bool
+ dataProvider   *dataproviders.DataProvider
+}
+
+func showTask(t *dataproviders.Task) {
+ var details string
+ if t.Details.Valid {
+  details = t.Details.String
+ } else {
+  details = "no details"
+ }
+ var deadline string
+ if t.Deadline.Valid {
+  year := t.Deadline.Time.Year()
+  month := t.Deadline.Time.Month().String()
+  day := t.Deadline.Time.Day()
+  deadline = fmt.Sprintf("%v %v %v", day, month, year)
+ } else {
+  deadline = "no deadline"
+ }
+ var completedAt string
+ if t.CompletedAt.Valid {
+  year := t.CompletedAt.Time.Year()
+  month := t.CompletedAt.Time.Month().String()
+  day := t.CompletedAt.Time.Day()
+  completedAt = fmt.Sprintf("%v %v %v", day, month, year)
+ } else {
+  completedAt = "wasn't completed"
+ }
+ fmt.Printf("id: %v, title: %v\ndetails: %v\ndeadline: %v, expired: %v\ncompleted: %v, completedAt: %v\n", t.Id, t.Title, details, deadline, t.Expired, t.Completed, completedAt)
+ fmt.Println("----------------------------------------------------------------")
+}
+
+func showTasks(tasks []*dataproviders.Task) {
+ for _, task := range tasks {
+  showTask(task)
+ }
 }
 
 func (sc *showTasksCommand) Run(args []string) error {
-	err := sc.fs.Parse(args)
-	if err != nil {
-		return err
-	}
-	ifAny := sc.all || sc.todo || sc.exp
-	if !ifAny || sc.all {
-		// TODO: implement "show all tasks" scenario
-		fmt.Println("all")
-	}
-	if sc.todo {
-		// TODO: implement "show active tasks" scenario
-		fmt.Println("uncompleted")
-	}
-	if sc.exp {
-		// TODO: implement "show expired tasks" scenario
-		fmt.Println("expire")
-	}
-	return nil
+ err := sc.fs.Parse(args)
+ if err != nil {
+  return err
+ }
+ ifAny := sc.all  sc.todo  sc.exp
+ if !ifAny || sc.all {
+  tasks, err := sc.dataProvider.ListAllTasks()
+  if err != nil {
+   return err
+  }
+  showTasks(tasks)
+ }
+ if sc.todo {
+  tasks, err := sc.dataProvider.ListActiveTasks()
+  if err != nil {
+   return err
+  }
+  showTasks(tasks)
+ }
+ if sc.exp {
+  tasks, err := sc.dataProvider.ListExpiredTasks()
+  if err != nil {
+   return err
+  }
+  showTasks(tasks)
+ }
+ return nil
 }
 
 func (sc *showTasksCommand) Name() string {
-	return sc.fs.Name()
+ return sc.fs.Name()
 }
 
-func NewShowTasksCommand() *showTasksCommand {
-	sc := &showTasksCommand{
-		fs: *flag.NewFlagSet("show", flag.ContinueOnError),
-	}
-	sc.fs.BoolVar(&sc.all, "all", false, "all tasks")
-	sc.fs.BoolVar(&sc.todo, "todo", false, "uncompleted tasks")
-	sc.fs.BoolVar(&sc.exp, "exp", false, "expired tasks")
-	return sc
+func NewShowTasksCommand(dp *dataproviders.DataProvider) *showTasksCommand {
+ sc := &showTasksCommand{
+  fs:           *flag.NewFlagSet("show", flag.ContinueOnError),
+  dataProvider: dp,
+ }
+ sc.fs.BoolVar(&sc.all, "all", false, "all tasks")
+ sc.fs.BoolVar(&sc.todo, "todo", false, "uncompleted tasks")
+ sc.fs.BoolVar(&sc.exp, "exp", false, "expired tasks")
+ return sc
 }
